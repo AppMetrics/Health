cd03b3e9db8ef3ddf123b975d32e6115e148ad46
alhardy <al_hardy@live.com.au>
2017-07-29 01:00:46 +1000

    [build fix] fixing broken health formatter collection test

--- test/App.Metrics.Health.Facts/Formatters/HealthFormatterCollectionTests.cs
+++ test/App.Metrics.Health.Facts/Formatters/HealthFormatterCollectionTests.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections.Generic;
+﻿// <copyright file="HealthFormatterCollectionTests.cs" company="Allan Hardy">
+// Copyright (c) Allan Hardy. All rights reserved.
+// </copyright>
+
+using System.Collections.Generic;
 using App.Metrics.Health.Formatters;
 using App.Metrics.Health.Formatters.Ascii;
 using App.Metrics.Health.Formatters.Json;
@@ -129,7 +133,7 @@ namespace App.Metrics.Health.Facts.Formatters
         public void Returns_default_when_attempting_to_get_type_with_mediatype_not_added()
         {
             // Arrange
-            var mediaType = new AppMetricsHealthMediaTypeValue("application", "vnd.appmetrics.health", "v1", "json");
+            var mediaType = new AppMetricsHealthMediaTypeValue("text", "vnd.appmetrics.health", "v1", "plain");
             var formatters =
                 new HealthFormatterCollection
                 {
